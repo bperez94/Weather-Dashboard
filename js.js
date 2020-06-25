d5346b16f78dcab83975ea126115fab7ba9c0691
Brandon Perez <brandonp940@gmail.com>
2020-06-25 18:59:19 -0400

    got api to work with user info

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
-console.log("hi");
+$(document).ready(function () {
+
+    var apiKey =  "f32d5b17f425f318c7f3a1cba88f2ccc";
+
+    //funcion for submit button
+    $("#submit").click(function (event) { 
+       event.preventDefault();
+      
+        
+        var cityName = $("#inputBox").val();
+
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName + "&appid=" + apiKey;
+    
+        // ajax get request
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
+            success:function(data) {
+            console.log(data)
+            }
+          })
+    
+      
+    
+    })
+})
