7ffcb2e8a24525e1bdff3acfffeb46d5366cd36a
Brandon Perez <brandonp940@gmail.com>
2020-06-27 21:51:10 -0400

    added funtions

--- js.js
+++ js.js
@@ -17,11 +17,31 @@ $(document).ready(function () {
             url: queryUrl,
             method: "GET",
             success:function(data) {
+                forcastRe(data)
             console.log(data)
             }
           })
-    
-      
-    
+          // function to send city names to search history
+          function searchHis() {
+              var listDiv = $("#listGroup");
+              var a = $("<a>");
+              a.text(cityName)
+              a.addClass("list-group-item list-group-item-action");
+              listDiv.prepend(a);
+          }
+          searchHis()
+          // grabs information about weather
+          function forcastRe(data) {
+             var cast = $("#forcast");
+             var p = $("<p>");
+             p.text(data.weather[0].description);
+             cast.append(p);
+          }
+          /* function to save cities to localstorage
+          localStorage.setItem("WeatherName",JSON.stringify(data)*/
+          
+          
+         
+
     })
 })
