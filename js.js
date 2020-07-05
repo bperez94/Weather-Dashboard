89bff24465851b8ea5ff82357a6d34b568920482
Brandon Perez <brandonp940@gmail.com>
2020-07-05 12:47:35 -0400

    added other weather info

--- js.js
+++ js.js
@@ -12,15 +12,19 @@ $(document).ready(function () {
         var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
         cityName + "&appid=" + apiKey;
     
-        // ajax get request
+        // ajax get request(single day)
         $.ajax({
             url: queryUrl,
             method: "GET",
             success:function(data) {
                 forcastRe(data)
-            console.log(data)
+            console.log(data);
             }
           })
+        // uv index api 
+        var queryUrl2 = "http://api.openweathermap.org/data/2.5/uvi?" + 
+        "appid=" + apiKey + cityName;
+        console.log(queryUrl2);  
           // function to send city names to search history
           function searchHis() {
               var listDiv = $("#listGroup");
@@ -28,18 +32,37 @@ $(document).ready(function () {
               a.text(cityName)
               a.addClass("list-group-item list-group-item-action");
               listDiv.prepend(a);
+            /*function to save cities to localstorage
+                localStorage.setItem("cities",JSON.stringify(cityName));
+                var cityStorage = localStorage.getItem("cities");
+                saves data but need it to populate city names searched */
+          
           }
           searchHis()
           // grabs information about weather
           function forcastRe(data) {
+            // section for sky
              var cast = $("#forcast");
              var p = $("<p>");
-             p.text(data.weather[0].description);
+             p.text(data.wind.speed);
              cast.append(p);
+            // section for humidity
+            var hum = $("#humidity");
+            var P1 = $("<p1>");
+            P1.text(data.main.humidity);
+            hum.append(P1);
+            // section for tempurature
+            var temp = $("#temp");
+            var p2 = $("<p2>");
+            p2.text(data.main.temp);
+            temp.append(p2);
+            // section for weather icon
+            var icon = $("#icon");
+            var p3 = $("<p3>");
+            p3.text(data.weather[0].icon);
+            icon.append(p3);
           }
-          /* function to save cities to localstorage
-          localStorage.setItem("WeatherName",JSON.stringify(data)*/
-          
+         
           
          
 
